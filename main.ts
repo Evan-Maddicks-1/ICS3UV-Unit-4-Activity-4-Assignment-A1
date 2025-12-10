4e99e1bc08247004b82c518599b3edf6164c45d2
default <evan.maddicks@stu.ocsb.ca>
2025-12-10 15:34:33 +0000

    finished code and functions properly

---
+++ main.ts
@@ -0,0 +1,28 @@
+/**
+* @author Evan Maddicks
+* @version 1.0.0
+* @date 2025-12-1
+* @fileoverview this program will simulate a simple guessing game
+*/
+//Prompt user
+console.log("Welcome to the Guessing Game!");
+console.log("Guess a number between 1 and 10. Enter 0 to quit.");
+
+let guess = Number(prompt("Enter your guess: "));
+
+while (guess !== 0) {
+  //Generate a random number between 1 and 10
+  let randomNumber = Math.floor(Math.random() * 10) + 1;
+
+  if (guess === randomNumber) {
+    console.log("Congratulations! You guessed the correct number: " + guess);
+    break;
+  } else {
+    console.log("Try again.");
+    guess = Number(prompt("Enter your guess: "));
+  }
+}
+
+if (guess === 0) {
+  console.log("Game ended. Thanks for playing!");
+}
